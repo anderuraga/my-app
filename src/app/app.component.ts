10978823e62c06b45907e9f19fc64b98a608e075
anderuraga <ander.uraga@gmail.com>
2018-11-27 09:54:11 +0100

    cambios basicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'my-app';
+
+  title = 'Primera App Angular';
+  autor = 'Ander';
+
+
 }
